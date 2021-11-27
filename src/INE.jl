6ca1a6b530c26be2e6aa232dd9dbcde0ebe61b3c
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2021-11-27 09:54:55 +0100

    Adapt to JSONStat

--- src/INE.jl
+++ src/INE.jl
@@ -1,6 +1,6 @@
 module INE
 
-using HTTP, JSON3, JSONStat
+using HTTP, JSONStat
 using Dates
 
 export INEdataset
@@ -9,11 +9,7 @@ function INEdataset(id::String; from::Date=Date(2010, 12))
     resp = HTTP.get("https://servicios.ine.es/wstempus/jsstat/ES/DATASET/" *
                     id *"?date=" * Dates.format(from, "Ymmdd") *":").body
 
-    js = JSON3.read(resp, JSONStat.Dataset)
-
-    println(js.label, "\n", "")
-
-    return JSONStat.datatable(js)
+    return JSONStat.parse(resp)
 end
 
 INEdataset(id::Int; from::Date=Date(2010, 12)) = INEdataset(string(id); from)
