fcc1d4795cfcd97f241db6473d5aed6a262e7dd3
lsaenzt <59137624+lsaenzt@users.noreply.github.com>
2021-11-27 23:32:51 +0100

    JSONStat.read

--- src/INE.jl
+++ src/INE.jl
@@ -9,9 +9,11 @@ function INEdataset(id::String; from::Date=Date(2010, 12))
     resp = HTTP.get("https://servicios.ine.es/wstempus/jsstat/ES/DATASET/" *
                     id *"?date=" * Dates.format(from, "Ymmdd") *":").body
 
-    return JSONStat.parse(resp)
+    return JSONStat.read(resp)
 end
 
 INEdataset(id::Int; from::Date=Date(2010, 12)) = INEdataset(string(id); from)
+
+function INEmetadata() end
 
 end # module
